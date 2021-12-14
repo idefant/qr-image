31c3dbea67eff7a6e8ba7cb2a8db77ea25e4a40e
idefant <inik3004@gmail.com>
2021-12-14 13:11:20 +0500

    add resizing image & transparent frame

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
 import os
 from amzqr import amzqr
+from PIL import Image, ImageOps
+
+
+img_size = 1024
+default_border_size = 7.5
+
+
+def get_image(filename):
+    img = Image.open(f'source/{filename}')
+    img_alpha = img.convert("RGBA")
+    return img_alpha
+
+
+def resize_image(img):
+    img.thumbnail((1024, 1024), Image.ANTIALIAS)
+
+
+def add_transparent_frame(filename, img, border_size):
+    img_border = ImageOps.expand(img, border=round(img_size * border_size / 100))
+    img_border.save(f'result/{filename}')
 
 
 def create_amazing_qr():
     print('- Source folder contains source images')
     print('- Result folder contains result images')
     print("Input a filename that exists and be tailed with one of {'.jpg', '.png', '.bmp', '.gif'}!")
-    text = input('Enter URL or Text: ')
-    input_filename = input('Enter input filename: ')
+    text = input('URL or Text: ')
+    filename = input('Filename: ')
+    border_size = input("Border size (in percent): ")
+    border_size = float(border_size) if border_size else default_border_size
+
+    img = get_image(filename)
+    resize_image(img)
+    # img.save(f'test.png')
+
+    add_transparent_frame(filename, img, border_size)
 
     version, level, qr_name = amzqr.run(
         words=text,
-        picture=f'source/{input_filename}',
+        picture=f'result/{filename}',
         colorized=True,
-        save_name=f'result\\{input_filename}',
+        save_name=f'result\\{filename}',
         save_dir=os.getcwd()
     )
 
